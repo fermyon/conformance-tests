273f6f6187e3ef7b53fdea9c3f50b51ea7b53766
Ryan Levick <ryan.levick@fermyon.com>
2024-05-23 11:13:39 +0200

    Add CI
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- tests/request-shape/src/lib.rs
+++ tests/request-shape/src/lib.rs
@@ -3,7 +3,7 @@ use anyhow::Context;
 mod bindings {
     wit_bindgen::generate!({
             world: "http-trigger",
-            path:  "../wit",
+            path:  "../../wit",
     });
     use super::Component;
     export!(Component);
@@ -46,15 +46,15 @@ fn check_method(req: &IncomingRequest) -> anyhow::Result<()> {
 }
 
 fn check_url(req: &IncomingRequest) -> anyhow::Result<()> {
-    let authority = req.authority();
-    let expected = "127.0.0.1:3000";
-    anyhow::ensure!(
-        authority.as_deref() == Some(expected),
-        "URL was expected to be '{expected}' but was '{authority:?}'"
-    );
+    let authority = req
+        .authority()
+        .context("incoming request has no authority")?;
+    let _addr: std::net::SocketAddr = authority
+        .parse()
+        .context("authority is not a valid SocketAddr")?;
 
     let path_with_query = req.path_with_query();
-    let expected = "/base/path?query=string";
+    let expected = "/base/path?key=value";
     anyhow::ensure!(
         path_with_query.as_deref() == Some(expected),
         "URL was expected to be '{expected}' but was '{path_with_query:?}'"
@@ -74,14 +74,14 @@ fn check_headers(req: &IncomingRequest) -> anyhow::Result<()> {
     // Check that the headers are as expected
     let headers = [
         ("spin-raw-component-route", "/..."),
-        ("spin-full-url", "http://example.com/base/path?query=string"),
+        ("spin-full-url", "http://example.com/base/path?key=value"),
         ("spin-path-info", "/path"),
         ("spin-base-path", "/base"),
         ("spin-component-route", ""),
     ];
 
     for (name, value) in headers.into_iter() {
-        let header = header_as_string(&req, name)?;
+        let header = header_as_string(req, name)?;
 
         anyhow::ensure!(
             header == value,
@@ -90,7 +90,7 @@ fn check_headers(req: &IncomingRequest) -> anyhow::Result<()> {
     }
 
     // Check that the spin-client-addr header is a valid SocketAddr
-    let _: std::net::SocketAddr = header_as_string(&req, "spin-client-addr")?
+    let _: std::net::SocketAddr = header_as_string(req, "spin-client-addr")?
         .parse()
         .context("spin-client-addr header is not a valid SocketAddr")?;
 
@@ -98,7 +98,7 @@ fn check_headers(req: &IncomingRequest) -> anyhow::Result<()> {
 }
 
 /// Fails unless there is exactly one header with the given name, and it is valid UTF-8
-fn header_as_string<'a>(req: &'a IncomingRequest, name: &str) -> anyhow::Result<String> {
+fn header_as_string(req: &IncomingRequest, name: &str) -> anyhow::Result<String> {
     let mut headers = req.headers().get(&name.to_owned());
 
     if headers.len() != 1 {

05bf14d554ba2f6d748aea24b2641a15a86c2290
Ryan Levick <ryan.levick@fermyon.com>
2024-06-11 18:52:32 +0200

    Add ability to send request
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- crates/test-environment/src/http.rs
+++ crates/test-environment/src/http.rs
@@ -2,10 +2,13 @@
 
 use std::collections::HashMap;
 
+use anyhow::Context as _;
+
 /// A request to the Spin server
+#[derive(Debug, Clone)]
 pub struct Request<'a, B> {
     pub method: Method,
-    pub uri: &'a str,
+    pub path: &'a str,
     pub headers: &'a [(&'a str, &'a str)],
     pub body: Option<B>,
 }
@@ -15,7 +18,7 @@ impl<'a, 'b> Request<'a, &'b [u8]> {
     pub fn new(method: Method, uri: &'a str) -> Self {
         Self {
             method,
-            uri,
+            path: uri,
             headers: &[],
             body: None,
         }
@@ -26,25 +29,96 @@ impl<'a, B> Request<'a, B> {
     /// Create a new request with headers and a body
     pub fn full(
         method: Method,
-        uri: &'a str,
+        path: &'a str,
         headers: &'a [(&'a str, &'a str)],
         body: Option<B>,
     ) -> Self {
         Self {
             method,
-            uri,
+            path,
             headers,
             body,
         }
     }
 }
 
+impl<'a, B: Into<reqwest::Body>> Request<'a, B> {
+    /// Send the request to the given host and port
+    pub fn send(self, host: &str, port: u16) -> anyhow::Result<Response> {
+        let mut outgoing = reqwest::Request::new(
+            self.method.into(),
+            reqwest::Url::parse(&format!("http://{host}:{port}"))
+                .unwrap()
+                .join(self.path)
+                .context("could not construct url for request against Spin")?,
+        );
+        outgoing
+            .headers_mut()
+            .extend(self.headers.iter().map(|(k, v)| {
+                (
+                    reqwest::header::HeaderName::from_bytes(k.as_bytes()).unwrap(),
+                    reqwest::header::HeaderValue::from_str(v).unwrap(),
+                )
+            }));
+        *outgoing.body_mut() = self.body.map(Into::into);
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()?;
+        rt.block_on(async {
+            let mut retries = 0;
+            let mut response = loop {
+                let Some(request) = outgoing.try_clone() else {
+                    break reqwest::Client::new().execute(outgoing).await;
+                };
+                let response = reqwest::Client::new().execute(request).await;
+                if response.is_err() && retries < 5 {
+                    tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+                    retries += 1;
+                } else {
+                    break response;
+                }
+            }?;
+            let mut chunks = Vec::new();
+            while let Some(chunk) = response.chunk().await? {
+                chunks.push(chunk.to_vec());
+            }
+            Result::<_, anyhow::Error>::Ok(Response::full(
+                response.status().as_u16(),
+                response
+                    .headers()
+                    .into_iter()
+                    .map(|(k, v)| {
+                        (
+                            k.as_str().to_owned(),
+                            v.to_str().unwrap_or("<non-utf8>").to_owned(),
+                        )
+                    })
+                    .collect(),
+                chunks,
+            ))
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum Method {
     Get,
     Post,
     Put,
     Patch,
     Delete,
+}
+
+impl Into<reqwest::Method> for Method {
+    fn into(self) -> reqwest::Method {
+        match self {
+            Method::Get => reqwest::Method::GET,
+            Method::Post => reqwest::Method::POST,
+            Method::Put => reqwest::Method::PUT,
+            Method::Patch => reqwest::Method::PATCH,
+            Method::Delete => reqwest::Method::DELETE,
+        }
+    }
 }
 
 /// A response from a Spin server

78099af22075b316600a888048bc71f2b266b463
Ryan Levick <ryan.levick@fermyon.com>
2024-06-11 18:56:09 +0200

    Add test_environment
    Signed-off-by: Ryan Levick <ryan.levick@fermyon.com>

--- crates/test-environment/src/lib.rs
+++ crates/test-environment/src/lib.rs
@@ -1,3 +1,14 @@
-mod http;
 mod io;
-mod services;
+mod test_environment;
+
+pub mod http;
+pub mod services;
+
+// A runtime which can be tested
+pub trait Runtime {
+    /// Return an error if the runtime has errored
+    fn error(&mut self) -> anyhow::Result<()>;
+}
+
+#[doc(inline)]
+pub use test_environment::{RuntimeCreator, TestEnvironment, TestEnvironmentConfig};
